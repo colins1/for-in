a3b52879d2f0b8893819e7e925514d9edc46dd6d
colins1 <maxrozhkov1@gmail.com>
2022-06-25 17:30:55 +0300

    add

--- src/js/date.js
+++ src/js/date.js
@@ -1,1 +1,2 @@
-export default obj = {name: 'мечник', health: 10, level: 2, attack: 80, defence: 40}
+const obj = {name: 'мечник', health: 10, level: 2, attack: 80, defence: 40}
+export default obj
--- src/js/sort.js
+++ src/js/sort.js
@@ -1,5 +1,32 @@
-import obj from './date';
+export default function orderByProps(obj, arr) {
+    const {name, level, ...rest} = obj
 
-export default obj1 = obj
+    class Sort {
+        constructor (name, level) {
+            this.name = name
+            this.level = level
+        }
+    }
+    
+    const sort = new Sort(obj[arr[0]], obj[arr[1]])
+    const ordered = {};
+    Object.keys(rest).sort().forEach(function(key) {
+        ordered[key] = rest[key];
+    });
 
-console.log(obj1)
+    const objectAssign = Object.assign(sort, ordered);
+    const objecResult = []
+    for (let key in objectAssign) {
+        objecResult.push(
+            {key: key, value: objectAssign[key]}, // порядок взят из массива с ключами
+        )
+    }
+
+    return objecResult;
+
+
+
+    
+}
+
+//console.log(obj)
